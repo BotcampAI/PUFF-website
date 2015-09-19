e4a1aa4afc1ec393ae94816954558e7138794d5c
Valentine Nesterov <nesterov.valentine@gmail.com>
2015-09-19 16:35:22 +0300

    Add css build process.

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+    var autoprefixer = require('autoprefixer-core');
+    var cssnano = require('cssnano');
+    var postcssImport = require('postcss-import');
+
+    grunt.loadNpmTasks('grunt-postcss');
+
+    grunt.initConfig({
+        postcss: {
+            options: {
+                map: true,
+                processors: [
+                    autoprefixer({
+                        browsers: ['last 2 versions']
+                    }),
+                    postcssImport({
+                        path: ['css/'],
+                    }),
+                    cssnano()
+                ]
+            },
+            dist: {
+                src: 'css/*.css',
+                dest: 'build/css/main.css'
+            }
+        }
+    });
+
+    grunt.registerTask('build', ['postcss']);
+};
